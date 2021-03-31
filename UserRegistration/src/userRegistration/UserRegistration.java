8628f5b96a3fcc612424dc3a20abc547882c94ca
Sainath470 <akulavenkatasainath1997@gmail.com>
2021-03-31 22:52:59 +0530

    Added: User validation method to check first name

--- UserRegistration/src/userRegistration/UserRegistration.java
+++ UserRegistration/src/userRegistration/UserRegistration.java
@@ -1,7 +1,43 @@
 package userRegistration;
+import java.util.regex.*;
+import java.util.Scanner;
 public class UserRegistration{
-	public static void main(String args[])
+	String firstName;
+	Scanner input=new Scanner(System.in);
+
+	//Method for taking user Input
+	public String getFirstName()
 	{
-		System.out.println("Welcome to the User Registration");
+		System.out.println("Please Enter your first Name");	
+		System.out.println("Rules: ");
+		System.out.println("First Name must start with Capital letter");
+		System.out.println("Contain minimum 3 characters");
+		String name=input.next();
+		return name;
 	}
+
+	//Method for input checking
+	public void userValidator()
+	{
+		this.firstName=getFirstName();
+		Boolean validate=Pattern.matches("^[A-Z][a-z]{2,}", firstName);
+		if(Boolean.TRUE.equals(validate))
+		{
+			System.out.println("VALID");
+		}
+		else
+		{
+			System.out.println("INVALID");
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the User Registration");
+		UserRegistration userinput=new UserRegistration();
+		userinput.userValidator();
+
+	}
+
 }
+
+
